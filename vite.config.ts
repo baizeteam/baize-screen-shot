5f5ff3718e7595a9db3f8d8ce2f59e16ae79e71b
xiaoyu <xiaoyu@sulg.top>
2024-04-26 00:37:56 +0800

    ✨ 添加ts自动生成插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
@@ -8,4 +9,5 @@ export default defineConfig({
       fileName: "index",
     },
   },
+  plugins: [dts()],
 });
